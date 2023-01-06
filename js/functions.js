f217ebbfb34eb21b21da717d9feed05201aeb22c
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-01-06 14:06:18 -0300

    funcao criptograf

--- js/functions.js
+++ js/functions.js
@@ -6,4 +6,27 @@ function contraste() {
 
 };
 
+const noContent = document.querySelector("#sem_conteudo");
+const content = document.querySelector("#conteudo_cifrado");
+
+function crypto() {
+
+    var texto = document.getElementById("message").value.toLowerCase();
+
+    if(texto == ""){
+        alert("Insira antes uma mensagem!")
+    }
+    else {
+        var textoCifrado = texto.replace(/a/igm,"ai");
+        var textoCifrado = textoCifrado.replace(/e/igm,"enter");
+        var textoCifrado = textoCifrado.replace(/i/igm,"imes");
+        var textoCifrado = textoCifrado.replace(/o/igm,"ober");
+        var textoCifrado = textoCifrado.replace(/u/igm,"ufat");
+
+        noContent.classList.add("oculto");
+        content.classList.remove("oculto");
+
+        document.getElementById("conteudo_cifrado").innerHTML = textoCifrado;
+    }
+};
 
